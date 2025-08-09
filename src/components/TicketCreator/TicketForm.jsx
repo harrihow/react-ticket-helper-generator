bcdead4718d8552894c196f0d98d05c1fc6f880b
harrifano1997 <richanne.harri@gmail.com>
2025-08-10 02:25:24 +0800

    Added useState for Ticket No. Field

--- src/components/TicketCreator/TicketForm.jsx
+++ src/components/TicketCreator/TicketForm.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useState } from "react";
 import CaseDetailsSection from "./sections/CaseDetailsSection";
 import CallerSection from "./sections/CallerSection";
